5d41a64f56f46157cae8c4c6018927280e334e6c
Kumaran Venkataraman <me@kumaranvram.com>
2014-03-20 16:29:49 +0530

    Added Main file

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import com.lasercode.parser.JsonFlattener;
+import com.lasercode.printer.CsvWriter;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        JsonFlattener parser = new JsonFlattener();
+        CsvWriter writer = new CsvWriter();
+
+        List<Map<String, String>> flatJson = parser.parseJson(jsonValue());
+        writer.writeAsCSV(flatJson, "sample.csv");
+    }
+
+    private static String jsonValue() {
+        return "[\n" +
+                "    {\n" +
+                "        \"studentName\": \"Foo\",\n" +
+                "        \"Age\": \"12\",\n" +
+                "        \"subjects\": [\n" +
+                "            {\n" +
+                "                \"name\": \"English\",\n" +
+                "                \"marks\": \"40\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"name\": \"History\",\n" +
+                "                \"marks\": \"50\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"studentName\": \"Bar\",\n" +
+                "        \"Age\": \"12\",\n" +
+                "        \"subjects\": [\n" +
+                "            {\n" +
+                "                \"name\": \"English\",\n" +
+                "                \"marks\": \"40\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"name\": \"History\",\n" +
+                "                \"marks\": \"50\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"name\": \"Science\",\n" +
+                "                \"marks\": \"40\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"studentName\": \"Baz\",\n" +
+                "        \"Age\": \"12\",\n" +
+                "        \"subjects\": []\n" +
+                "    }\n" +
+                "]";
+    }
+}
